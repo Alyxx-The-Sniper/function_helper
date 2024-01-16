e7b778731dc8c6f92cd0a7da6ab02f992b52be24
alyxx <75878508+Alyxx-The-Sniper@users.noreply.github.com>
2024-01-16 15:40:38 +0800

    Update function_helper.py
    add pred and store

--- function_helper.py
+++ function_helper.py
@@ -404,3 +404,43 @@ def split_dataset_with_classes(source_folder, cat_split_folder, min_samples=10, 
     print(f"Split dataset into train and test sets. Train set: {train_folder}, Test set: {test_folder}.")
 
 #################################################################################################
+
+
+# Create a function to return a list of dictionaries with sample, label, prediction, pred prob
+from tqdm.auto import tqdm
+def pred_and_store(test_paths, model, transform, class_names, device):
+  test_pred_list = []
+  for path in tqdm(test_paths):
+    # Create empty dict to store info for each sample
+    pred_dict = {}
+
+    # Get sample path
+    pred_dict["image_path"] = path
+
+    # Get class name
+    class_name = path.parent.stem
+    pred_dict["class_name"] = class_name
+
+    # Get prediction and prediction probability
+    from PIL import Image
+    img = Image.open(path) # open image
+    transformed_image = transform(img).unsqueeze(0) # transform image and add batch dimension
+    model.eval()
+    with torch.inference_mode():
+      pred_logit = model(transformed_image.to(device))
+      pred_prob = torch.softmax(pred_logit, dim=1)
+      pred_label = torch.argmax(pred_prob, dim=1)
+      pred_class = class_names[pred_label.cpu()]
+
+      # Make sure things in the dictionary are back on the CPU
+      pred_dict["pred_prob"] = pred_prob.unsqueeze(0).max().cpu().item()
+      pred_dict["pred_class"] = pred_class
+
+    # Does the pred match the true label?
+    pred_dict["correct"] = class_name == pred_class
+
+    # print(pred_dict)
+    # Add the dictionary to the list of preds
+    test_pred_list.append(pred_dict)
+
+  return test_pred_list
